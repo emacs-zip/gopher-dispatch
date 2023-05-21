210bdfd04d1551fb75b215dec062ca727cb34d95
veasman <charlie@moren.dev>
2023-05-21 01:12:37 -0500

    style: remove commented code block

--- api/middleware/jwt.go
+++ api/middleware/jwt.go
@@ -47,33 +47,3 @@ func AuthRequired() gin.HandlerFunc {
         c.Next()
     }
 }
-
-/*func AuthRequired() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        const BearerSchema = "Bearer "
-        authHeader := c.GetHeader("Authorization")
-        token := authHeader[len(BearerSchema):]
-
-        decodedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
-            if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-                return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-            }
-
-            return []byte(secret), nil
-        })
-
-        if err != nil {
-            c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
-            return
-        }
-
-        if claims, ok := decodedToken.Claims.(jwt.MapClaims); ok && decodedToken.Valid {
-            c.Set("userID", claims["user_id"].(string))
-        } else {
-            c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
-            return
-        }
-
-        c.Next()
-    }
-}*/
